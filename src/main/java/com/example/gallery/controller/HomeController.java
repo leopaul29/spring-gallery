caac74f6eae44f0fc19da81a6abd0446945b6f64
Leo-paul <leopaulmartin@hotmail.fr>
2021-02-26 23:49:26 +0100

    update home controller

--- src/main/java/com/example/gallery/controller/HomeController.java
+++ src/main/java/com/example/gallery/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.gallery.controller;
 
 import com.example.gallery.data.Album;
+import com.example.gallery.data.Photo;
 import com.example.gallery.service.AlbumConsumerService;
 import com.example.gallery.service.AlbumConsumerServiceImpl;
+import com.example.gallery.service.PhotoConsumerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,27 +25,29 @@ public class HomeController {
 
     @Autowired
     private AlbumConsumerService albumConsumerService;
+    @Autowired
+    private PhotoConsumerService photoConsumerService;
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
 
-    @RequestMapping("/")
+    @RequestMapping(path = {"/", "/albums"})
     public String home(Model model) {
-        /*final String uri = "https://jsonplaceholder.typicode.com/albums";
-        RestTemplate restTemplate = new RestTemplate();
-
-
-
-        String albumList = restTemplate.getForObject(uri, String.class);
-
-*/
         List<Album> albumList = albumConsumerService.processAlbumDataFromAlbumArray();
-
-
-        log.info(albumList.toString());
         model.addAttribute("albumList", albumList);
         return "home";
+    }
+
+    @RequestMapping("/albums/{id}")
+    public String album(Model model, @PathVariable Long id) {
+        Album album = albumConsumerService.processAlbumDataFromAlbum(id);
+        model.addAttribute("album", album);
+
+        List<Photo> photos = photoConsumerService.processPhotoDataFromAlbum(id);
+        model.addAttribute("photos", photos);
+
+        return "album";
     }
 }
